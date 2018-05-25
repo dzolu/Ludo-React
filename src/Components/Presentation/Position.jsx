554ec90d56ec35b625ac0d441d6cc881720627ed
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-25 17:57:52 +0100

    declare new props on props on position and add style to position

--- src/Components/Presentation/Position.jsx
+++ src/Components/Presentation/Position.jsx
@@ -1,9 +1,15 @@
-import React from "react"; 
+import React from "react";
+import PropTypes from 'prop-types';
 
 const Position=(props)=>{
+    return  (<div className={`position position--${props.color} position--${props.type}`}></div>)
+}
 
-    return <div>
-        Position
-    </div>
+Position.propTypes={
+    pawn: PropTypes.object,
+    color:PropTypes.string.isRequired,
+    type:PropTypes.string.isRequired,
+    positionref:  PropTypes.object.isRequired,
+    id:  PropTypes.number.isRequired
 }
 export default Position;
