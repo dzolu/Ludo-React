3b0faa98fff3ca129ac4ce1ecd26146f97f9b708
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-25 17:56:46 +0100

    create color type and position type

---
+++ src/Types/ColorTypes.js
@@ -0,0 +1,6 @@
+export const COLOR_BLUE="blue";
+export const COLOR_RED="red";
+export const COLOR_GREEN="green";
+export const COLOR_YELLOW="yellow";
+export const COLOR_WHITE="white";
+export const COLOR_TRANSPARENT="transparent";
