5ca89fc98319b719cdd2aab70bc8fae3aa976fb0
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-29 15:53:33 +0100

    add redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux'
 import Board from './Components/Presentation/Board';
-
+import { configureStore } from './store/configureStore';
+const store = configureStore()
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <Board/>
       </div>
+      </Provider>
     );
   }
 }
