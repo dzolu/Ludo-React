6c58f0cf5e8373c93f1e45f0ccd45852c3767dd7
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-06-14 13:37:05 +0100

    clear actions after moves

--- src/core/Moves/index.js
+++ src/core/Moves/index.js
@@ -1,19 +1,25 @@
-class Moves{
-    static makeMove(props){
-         if (props.actionsList && props.actionsList.length) {
-           // props.actions.nextPlayer(this.nextPlayer(props));  
-                props.actionsList.forEach(action => {
-            props.actions.dispatchAction(action.type, action.pawn);
-           });       
+class Moves {
+    static makeMove(props) {
+        if (props.actionsList && props.actionsList.length) {
+            props.actionsList.forEach(action => {
+                props.actions.dispatchAction(action.type, action.pawn);
+            });
+            props.actions.nextPlayer(Moves.nextPlayer(props));
         }
     }
-    nextPlayer=(props)=>{
-        return Object.keys(props.pawns).map(key=>{
-            const pawn=props.pawns[key];
-            return pawn.id===props.id ? {...pawn, positionType: props.positionType , positionIndex: props.positionIndex,actions: []} : {...pawn, actions: []};
-       });
-    
-       }      
+    static nextPlayer = (props) => {
+        return Object.keys(props.pawns).map(key => {
+            const pawn = props.pawns[key];
+            return pawn.id === props.id ? { ...pawn,
+                positionType: props.positionType,
+                positionIndex: props.positionIndex,
+                actions: []
+            } : { ...pawn,
+                actions: []
+            };
+        });
+
+    }
 }
 
 export default Moves;
