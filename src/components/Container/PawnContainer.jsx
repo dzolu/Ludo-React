5130811c606996449b969fe75b0cc7ff3e737943
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-06-19 15:01:52 +0100

    cleanup pawnContainer

--- src/components/Container/PawnContainer.jsx
+++ src/components/Container/PawnContainer.jsx
@@ -3,25 +3,9 @@ import {connect} from "react-redux";
 import * as pawnActions from "../../redux/actions/pawnActions";
 import {bindActionCreators} from "redux";
 import Pawn from '../Presentation/Pawn';
-import {TYPE_BOARD} from "../../types/PositionTypes";
 import Application from '../../core/Application'
-import actions from '../../redux/reducers/actionsReducer';
 const PawnContainer = (props) => {
-    const nextPlayer=()=>{
-       return Object.keys(props.pawns).map(key=>{
-           const pawn=props.pawns[key];
-           return pawn.id===props.id ? {...pawn, positionType: props.positionType , positionIndex: props.positionIndex,actions: []} : {...pawn, actions: []};
-      });
-   
-      }
     const move = () => {
-        // if (props.actionName && props.actionName !== "") {
-        //     props.actions.nextPlayer(nextPlayer());  
-        //     props.actions.dispatchAction(props.actionName, props);
-        //    if(props.positionType===TYPE_BOARD){
-        //         props.actions.clearAfterMove(props);  
-        //    }       
-        // }
         Application.move(props);
     };
    
