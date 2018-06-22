048ad6ff1ea9ae0790df77422bcddf8baea9989e
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-06-22 15:37:38 +0100

    add home index for all colors

--- src/AppSettings.js
+++ src/AppSettings.js
@@ -1,4 +1,8 @@
 export const HOME_INDEX="HOME_INDEX";
 export const TOTAL_POSITION_ON_BOARD=39
 export const RED_HOME_INDEX=0;
+export const GREEN_HOME_INDEX=10;
+export const YELLOW_HOME_INDEX=20;
+export const BLUE_HOME_INDEX=30;
+
 
