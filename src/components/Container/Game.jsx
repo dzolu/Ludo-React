15b0d0970d250159d63b812dd3822339197e0350
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-06-26 12:27:20 +0100

    add logic for next player when plater don't have a move after throw

--- src/components/Container/Game.jsx
+++ src/components/Container/Game.jsx
@@ -4,10 +4,37 @@ import DiceContainer from "./DiceContainer";
 import { ToastContainer } from "react-toastify";
 import {connect} from "react-redux";
 import Notification from "../../core/Notifications";
-import Messages from '../../core/Messages';
+import Message from '../../core/Messages';
+import Queue from '../../core/Queue';
+import { UNABLE_TO_MOVE } from '../../redux/actions/actionTypes';
+import * as pawnActions from "../../redux/actions/pawnActions";
+import {bindActionCreators} from "redux";
+
 class Game extends Component{
         componentDidMount() {
-          Notification.notifyInfo(Messages.gameBegin(this.props));
+          Notification.notifyInfo(Message.gameBegin(Queue.first(this.props.queue)));
+        }
+        componentDidUpdate(prevProps){
+            const {queue, actions, result}=this.props;
+            const player=Queue.first(queue)
+            const prevPlayer= Queue.first(prevProps.queue)
+           if(player.id !== prevPlayer.id){
+               return;
+           }
+            if(player.pawns.filter(this.unableToMoveFilter).length){
+                return;
+            }
+            Notification.notifyInfo(Message.unableToMove({player, result}));
+            const queueNew = Queue.add({queue:Queue.remove(queue), player}); 
+            actions.nextPlayer(queueNew);
+            Notification.notifyInfo(Message.nextPlayer(Queue.first(queueNew)));
+        }
+      
+        unableToMoveFilter(pawn){
+            if(!pawn.actions || !pawn.actions.length ){
+                return true;
+            }
+           return pawn.actions && pawn.actions[0].type!==UNABLE_TO_MOVE;
         }
         render(){
             return (
@@ -27,9 +54,14 @@ class Game extends Component{
 }
 function mapStateToProps(state) {
     return {
-        queue: state.queue, 
+        queue: state.queue,
+        result: state.diceResult 
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(pawnActions, dispatch)
     }
 }
 
-
-export default connect(mapStateToProps)(Game);
+export default connect(mapStateToProps,mapDispatchToProps)(Game);
