d409012bdcea1579363d9e4c90d624a9d695c7a0
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-06-28 11:23:25 +0100

    write the test around move class

--- src/__tests__/moves.test.js
+++ src/__tests__/moves.test.js
@@ -1,14 +1,41 @@
 import 'jest'
 import Moves from '../core/Moves';
+import { UNABLE_TO_MOVE } from '../redux/actions/actionTypes';
+import Notification from '../core/Notifications';
 
 
 describe("move class test", ()=>{
     it("When call makeMove with empty queue then function is escape with no crash", ()=>{
         const props={
-            queue:[]
+            queue:[],
+            actions: {nextPlayer: jest.fn()} 
         }
         const spy= jest.spyOn(console,"error");
+        const spyNextPlayer= jest.spyOn(props.actions,"nextPlayer");
         Moves.makeMove(props);
         expect(spy).toHaveBeenCalled();
+        expect(spyNextPlayer).not.toHaveBeenCalled();
+    })
+
+    it("When call makeMove and pawn have an action type of UNABLE_TO_MOVE then the notyfication is ", ()=>{
+        const message="this is a message"
+        const pawnMock={
+            actions:[{           
+                    type: UNABLE_TO_MOVE,               
+                   pawn: {...{}, message}
+               }],
+               message
+       }
+        const player={
+            pawns:[pawnMock]
+        }
+      
+        const props={
+            id:0,
+            queue:[player]
+        }
+        var notificationSpy=jest.spyOn(Notification,"notifyError");
+        Moves.makeMove(props);
+        expect(notificationSpy).toHaveBeenCalledWith(message);
     })
 })
