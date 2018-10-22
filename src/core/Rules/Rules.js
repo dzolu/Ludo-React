7fc94ecda0a4636d621e6418fd4869e2a89afa59
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-10-22 23:24:53 +0200

    fix: leave the base logic

--- src/core/Rules/Rules.js
+++ src/core/Rules/Rules.js
@@ -1,37 +1,41 @@
-import  rules from "./index"
+import rules from "./index"
 import * as AppSettings from "./../../AppSettings";
 import {
     TYPE_BASE,
     TYPE_BOARD,
     TYPE_HOME
 } from "../../types/PositionTypes";
-import { TOTAL_POSITION_ON_BOARD } from '../../AppSettings';
-import { UNABLE_TO_MOVE } from '../../redux/actions/actionTypes';
+import {BLUE_START_INDEX, GREEN_START_INDEX, TOTAL_POSITION_ON_BOARD, YELLOW_START_INDEX} from '../../AppSettings';
+import {UNABLE_TO_MOVE} from '../../redux/actions/actionTypes';
 import allPawnsInhome from "../Filters/allInHomeFilter";
+import {COLOR_BLUE, COLOR_GREEN, COLOR_RED, COLOR_YELLOW} from "../../types/ColorTypes";
+import {RED_START_INDEX} from "./../../AppSettings";
 
-class Rules{
-    static check(props){
-        rules.forEach(rule=>{rule(props)})
+class Rules {
+    static check(props) {
+        rules.forEach(rule => {
+            rule(props)
+        })
     }
 
-    static isAbleToMove(pawn){
-        if(!pawn){
-            console.error("Wrong props. It does not contain the definition of pawn.");    
+    static isAbleToMove(pawn) {
+        if (!pawn) {
             return false;
         }
-        if(!pawn.actions || !pawn.actions.length){
+        if (!pawn.actions || !pawn.actions.length) {
             return true;
         }
-        return  pawn.actions[0].type!==UNABLE_TO_MOVE
+        return pawn.actions[0].type !== UNABLE_TO_MOVE
     }
+
     static isOnBoard(pawn) {
         return pawn && pawn.positionType === TYPE_BOARD;
     }
-    
+
     static isInHome(pawn) {
         return pawn && pawn.positionType === TYPE_HOME;
     }
-    
+
     static isInBase(pawn) {
         return pawn && pawn.positionType === TYPE_BASE;
     }
@@ -39,8 +43,9 @@ class Rules{
     static createActionType(color, name) {
         return color && name && `${color.toUpperCase()}_${name}`;
     }
+
     static getHome(props) {
-        if(!props || !props.pawn || !props.pawn.color){
+        if (!props || !props.pawn || !props.pawn.color) {
             console.error("Wrong props. It does not contain the definition of pawn.")
         }
         return props[`${props.pawn.color}Home`];
@@ -55,13 +60,29 @@ class Rules{
     }
 
     static ableToMoveHome(props) {
-        const home=Rules.getHome(props);
-        const {pawn}=props;
-        return !Rules.isInHome(pawn) && pawn.counterAfterMove > AppSettings.TOTAL_POSITION_ON_BOARD && pawn.counterAfterMove < 44 && !home[pawn.counterAfterMove - TOTAL_POSITION_ON_BOARD - 1 ]
-    }  
-    static gameOver(player){
-        return player.pawns.filter(allPawnsInhome).length===4;
-    } 
+        const home = Rules.getHome(props);
+        const {pawn} = props;
+        return !Rules.isInHome(pawn) && pawn.counterAfterMove > AppSettings.TOTAL_POSITION_ON_BOARD && pawn.counterAfterMove < 44 && !home[pawn.counterAfterMove - TOTAL_POSITION_ON_BOARD - 1]
+    }
+
+    static gameOver(player) {
+        return player.pawns.filter(allPawnsInhome).length === 4;
+    }
+
+    static GetStartIndex(color) {
+        switch (color) {
+            case COLOR_RED:
+                return RED_START_INDEX;
+            case COLOR_BLUE:
+                return BLUE_START_INDEX;
+            case COLOR_GREEN:
+                return GREEN_START_INDEX;
+            case COLOR_YELLOW:
+                return YELLOW_START_INDEX;
+            default:
+                return 0;
+        }
+    }
 
 }
 
--- src/core/Rules/leaveBase.js
+++ src/core/Rules/leaveBase.js
@@ -1,7 +1,8 @@
 import Rules from './Rules';
 import * as AppSettings from "./../../AppSettings";
 import * as Types from "../../redux/actions/actionTypes";
-import { TYPE_BOARD} from '../../types/PositionTypes';
+import {TYPE_BOARD} from '../../types/PositionTypes';
+
 export default function leaveBase(props) {
     const {
         pawn,
@@ -11,15 +12,20 @@ export default function leaveBase(props) {
     if (!Rules.isInBase(pawn)) {
         return;
     }
-    const startIndex=Rules.createActionType(pawn.color, AppSettings.START_INDEX);
+    const startIndex = Rules.GetStartIndex(pawn.color);
     if ((result === 1 || result === 6) && !gameBoard[startIndex]) {
         const type = Rules.createActionType(pawn.color, Types.LEAVE_BASE);
-        const newPawn=Object.assign({},{...pawn},{nextPositionType:TYPE_BOARD, counterAfterMove:0, nextPositionIndex: AppSettings[startIndex]});
+        const newPawn = {
+            ...pawn,
+            nextPositionType: TYPE_BOARD,
+            counterAfterMove: 0,
+            nextPositionIndex: AppSettings[startIndex]
+        };
         props.pawnActions.addAction({
-            pawn:newPawn,
+            pawn: newPawn,
             types: [{
                 type,
-                pawn:newPawn
+                pawn: newPawn
             }]
         });
     }
